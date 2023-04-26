b49c2cea2a149a9c967201f403f72dfb46de065b
Aaron M. Agosto <aaronagosto0613@gmail.com>
2023-04-26 16:58:08 -0400

    ADDED style

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,25 @@
+import { Github } from "react-bootstrap-icons"
+
 export default function Footer() {
 
- const gitHubUrl = ('https://github.com/aaronmagosto/profile-c10')
+ const gitHubUrl = 'https://github.com/aaronmagosto/profile-c10'
  const currentYear = new Date().getFullYear()
 
-  return(
+  return (
     <footer>
-    <p><small>&copy; {currentYear} </small> </p>
-    </footer>
+      <p className="text-center">
+        <a href= {gitHubUrl}
+        target="_blank"
+        rel="noreferrer"
+        className="button-effect">
+        <Github size='60' color='red'/>
+        </a>
+        <small>&copy; {currentYear} </small>
+        </p>
+        </footer>
+
+
+
   )
 
 }
--- src/components/Gallery/GalleryCard.jsx
+++ src/components/Gallery/GalleryCard.jsx
@@ -4,14 +4,14 @@ export default function  GalleryCard ({data: {id,name,content,website,cover}} ) 
 
 
   return (
-    <Col key={id} md={3}>
-    <Card>
+    <Col key={id} md={4}> 
+    <Card className="gallery-card button-effect">
       <figure>
         <a href={website}
         target="_blank"
         rel="noreferrer">
-
         <Card.Img 
+        
         src={cover}/>
         </a>
       </figure>
